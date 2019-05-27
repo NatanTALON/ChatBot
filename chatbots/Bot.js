5b2b830ff30ed87f036d3be3485ff82c9bd5394a
NatanTALON <natan.talon@gmail.com>
2019-05-27 09:46:22 +0200

    possibilité de se connecter à Discord et SMS en même temps

--- chatbots/Bot.js
+++ chatbots/Bot.js
@@ -4,6 +4,7 @@ const RiveScript = require('rivescript');
 const Discord = require('discord.js');
 
 const services = {
+	ALL: -1,
 	SMS: 0,
 	DISCORD: 1		// https://discordapp.com/oauth2/authorize?client_id=Bot_Client_ID&scope=bot&permissions=2048	clientId for botounet = 579288474964852737
 }
@@ -12,11 +13,31 @@ class Bot {
 
 	constructor(name, service, token, brain) {
 		this.name = name;
-		this.service = service;
-		this.token = token;
 		this.brain = new RiveScript({utf8: true});
+		this.app = [ {active: false}, {active: false} ];
+		/*
+		[
+			{
+				active: boolean
+				service: application
+				token: ___
+			}
+		]
+		*/
+		switch(service) {
+			case services.SMS:
+				this.app[services.SMS].active = true;
+				this.app[services.SMS].token = token;
+				break;
+			case services.DISCORD:
+				this.app[services.DISCORD].active = true;
+				this.app[services.DISCORD].token = token;
+				break;
+			default:
+				console.log(`Error on constructor: no such service ${service}`);
+		}
+
 		this.conversations = [];
-		this.app;
 		/*
 		[
 			{
@@ -34,43 +55,55 @@ class Bot {
 		this.loading_done = function () {
 			console.log("Bot has finished loading!");
 			this.brain.sortReplies();
-			this.connectToService();			
+			this.connectToService(services.ALL);			
 		}
 
 
-		this.connectToService = function () {
-			switch(this.service) {
+		this.connectToService = function (service) {
+			switch(service) {
 				case services.SMS:
-					this.connect_SMS();
+					if(this.app[services.SMS].active) {
+						this.connect_SMS();
+					}
 					break;
 				case services.DISCORD:
-					this.connect_DISCORD();
+					if(this.app[services.DISCORD].active){
+						this.connect_DISCORD();
+					}
+					break;
+				case services.ALL:
+					if(this.app[services.SMS].active) {
+						this.connect_SMS();
+					}
+					if(this.app[services.DISCORD].active){
+						this.connect_DISCORD();
+					}
 					break;
 				default:
-					console.log("Error: Unkown service");
+					console.log(`Error on connection: no such service ${service}`);
 			}
 		}
 
 
 		this.connect_SMS = function () {
-			this.app = express();
-			this.app.set('view engine', 'ejs');
+			this.app[services.SMS].service = express();
+			this.app[services.SMS].service.set('view engine', 'ejs');
 
-			this.app.use(bodyParser.urlencoded({ extended: false }))
-			this.app.use(bodyParser.json());
+			this.app[services.SMS].service.use(bodyParser.urlencoded({ extended: false }))
+			this.app[services.SMS].service.use(bodyParser.json());
 
-			this.app.get('/', function(req, res) {
+			this.app[services.SMS].service.get('/', function(req, res) {
 				res.render('login');
 			});
 
-			this.app.post('/', postOnHome.bind(this));
+			this.app[services.SMS].service.post('/', postOnHome.bind(this));
 			function postOnHome(req,res) {
 				let newConv = {userName: req.body.userName, messageList: []};
 				this.conversations.push(newConv);
 				res.render('conversation', {conv: newConv});
 			}
 
-			this.app.post('/conv', postOnConv.bind(this));
+			this.app[services.SMS].service.post('/conv', postOnConv.bind(this));
 			function postOnConv(req,res) {
 				let i = this.conversations.findIndex(function(elt) {
 					return elt.userName == req.body.userName;
@@ -91,31 +124,51 @@ class Bot {
 
 			}
 
-			this.app.listen(parseInt(this.token,10), () => console.log(`bot listening on ${this.token}`));
+			this.app[services.SMS].server = this.app[services.SMS].service.listen(parseInt(this.app[services.SMS].token,10), () => console.log(`bot listening on ${this.app[services.SMS].token}`));
 		}
 		
 
 
-		this.stopListen = function(){
-			if( this.service == 0){
-				this.app.close();
-			}
-			else if(this.service == 1){
-				this.app.destroy();
+		this.stopListen = function(service){
+			switch(service) {
+				case services.SMS:
+					if(this.app[services.SMS].active) {
+						this.app[services.SMS].server.close();
+						this.app[services.SMS].active = false;
+					}
+					break;
+				case services.DISCORD:
+					if(this.app[services.DISCORD].active){
+						this.app[services.DISCORD].service.destroy();
+						this.app[services.DISCORD].active = false;
+					}
+					break;
+				case services.ALL:
+					if(this.app[services.SMS].active) {
+						this.app[services.SMS].service.close();
+						this.app[services.SMS].active = false;
+					}
+					if(this.app[services.DISCORD].active){
+						this.app[services.DISCORD].service.destroy();
+						this.app[services.DISCORD].active = false;
+					}
+					break;
+				default:
+					console.log(`Error on destructor: no such service ${service}`);
 			}
 		}
 
 		this.connect_DISCORD = function () {
-			this.app = new Discord.Client();
+			this.app[services.DISCORD].service = new Discord.Client();
 
-			this.app.on('ready', () => {
+			this.app[services.DISCORD].service.on('ready', () => {
 				console.log(`Logged in as ${client.user.tag}!`);
 			});
 
-			this.app.on('message', botResponse.bind(this));
+			this.app[services.DISCORD].service.on('message', botResponse.bind(this));
 
 			function botResponse(msg) {
-				if(!msg.author.bot && msg.isMemberMentioned(this.app.user)) {
+				if(!msg.author.bot && msg.isMemberMentioned(this.app[services.DISCORD].service.user)) {
 					this.brain.reply(msg.author.discriminator, msg.content).then(sendResponse.bind(this));
 					function sendResponse(botMsg) {
 						msg.reply(botMsg);
@@ -123,7 +176,7 @@ class Bot {
 				}
 			}
 
-			this.app.login(this.token);
+			this.app[services.DISCORD].service.login(this.app[services.DISCORD].token);
 		}
 
 
@@ -144,10 +197,9 @@ class Bot {
 
 
 		this.changeService = function (service,token) {
-			this.stopListen();
-			this.service = service;
-			this.token = token;
-			this.connectToService();
+			this.stopListen(service);
+			this.app[service].token = token;
+			this.connectToService(service);
 		}
 
 
@@ -161,8 +213,8 @@ class Bot {
 
 
 /* test */
-//var bot = new Bot(0, 3001, undefined);
-//var bot = new Bot(1, 'NTc5Mjg4NDc0OTY0ODUyNzM3.XN__wg.5dkZA5O3uMyDbBySY0co-KljaIg', 'Botounet.rive');
+//bot = new Bot('Botounet', 0, 3001, 'standard.rive');
+//var bot = new Bot('Botounet', 1, 'NTc5Mjg4NDc0OTY0ODUyNzM3.XN__wg.5dkZA5O3uMyDbBySY0co-KljaIg', 'standard.rive');
 
 
 module.exports = Bot;
